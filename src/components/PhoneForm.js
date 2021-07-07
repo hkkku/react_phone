5beb36a93b47bff44da4423680d6cf4b129392d5
hkkku <hhk7045@gmail.com>
2021-07-07 20:33:11 +0900

    07/07 react 연습

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class PhoneForm extends Component {
+  state = {
+    name: "",
+    phone: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+      // input의 value를 name 값에 setState로 설정
+    });
+  };
+  // 이벤트 객체
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            placeholder="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          {/* onChange => input에서 값을 변경할 때마다 name 값이 바뀐다. */}
+          <input placeholder="phone number" value={this.state.phone} />
+          {/* input이 여러 개 생길 때의 state 관리법 */}
+          {this.state.name}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PhoneForm;
