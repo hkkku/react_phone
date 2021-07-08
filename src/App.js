77a91224cf2b2e17dcdcc1c9bca1954c709c08ac
hkkku <hhk7045@gmail.com>
2021-07-08 21:24:52 +0900

    0708 배열 삽입 component 진행

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
 
-function App() {
-  return (
-    <div>
-      <PhoneForm />
-    </div>
-  );
+class App extends Component {
+ 
+ id = 0;
+ // 초기값 정의   
+ // 렌더링 되는 값이 아니므로 setState 해줄 필요x 
+ state = {
+   information : [],
+   // 배열을 만들어주기
+ } 
+ 
+ handleCreate = (data) => {
+   //  data를 parameter로 갖는 함수 
+   //  console.log(data);
+
+   //  this.state.information.push(data);
+   // -> react에서는 불변성을 꼭 유지해줘야 한다
+   // -> 내부의 배열이나 객체를 수정할 때에는 새로운 배열이나 객체를 만들어서 값을 주입해주어야 한다. 
+    const { information } = this.state;
+    // 비구조 할당 문법 사용 -> 코드가 길어지는 걸 막는다. 
+    this.setState({
+      // information : this.state.information.concat(data),
+      information : information.concat(Object.assign({}, data,{
+        // ...data,
+
+        // name : data.name,
+        // phone: data.phone,
+        id : this.id++
+        //data가 추가될 때마다 id값이 1씩 상승
+        //값이 수정되었을때 리렌더링을 해주는 setState 
+      })),
+    })
+ }
+ 
+ render() {
+    return (
+      <div>
+        <PhoneForm onCreate = {this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+// 부모 컴포넌트에게 데이터를 전달할 자식 컴포넌트
 class PhoneForm extends Component {
   state = {
     name: "",
@@ -8,24 +8,43 @@ class PhoneForm extends Component {
 
   handleChange = (e) => {
     this.setState({
-      name: e.target.value,
+      [e.target.name] : e.target.value,
       // input의 value를 name 값에 setState로 설정
     });
-  };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onCreate(this.state);
+    this.setState({
+      // 값 초기화
+      name : '',
+      phone: '',
+    })
+  }
   // 이벤트 객체 조정
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
+            name = "name"
             placeholder="name"
             onChange={this.handleChange}
             value={this.state.name}
           />
           {/* onChange => input에서 값을 변경할 때마다 name 값이 바뀐다. */}
-          <input placeholder="phone number" value={this.state.phone} />
+          <input 
+            name="phone" 
+            placeholder="phone number" 
+            onChange={this.handleChange}
+            value={this.state.phone} />
           {/* input이 여러 개 생길 때의 state 관리법 */}
-          {this.state.name}
+          <button type ="submit">submit</button>
+          {/* <div>
+            {this.state.name}
+            {this.state.phone}
+          </div> */}
         </form>
       </div>
     );
