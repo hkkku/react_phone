c48ae9e99639b5b011ca74fde0d03bedbc130eb0
hkkku <hhk7045@gmail.com>
2021-07-14 21:00:32 +0900

    07/14 phoneinfo 수정기능 구현 & JSX expression 오류 수정중

--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class PhoneInfo extends Component {
   // 각 전화번호 정보를 보여주는 component
 
   state = {
     editing: false,
+    name: "",
+    phone: "",
   };
 
   handleRemove = () => {
@@ -13,12 +15,41 @@ class PhoneInfo extends Component {
   };
 
   handleToggleEdit = () => {
-    this.setState({});
+    // 함수가 한 번 실행될 때마다 반전
+    // true -> false
+    // = onUpdate
+    // false -> true
+    // = state에 info 값을 넣어주기
+    const { info, onUpdate } = this.props;
+    if (this.state.editing) {
+      onUpdate(info.id, {
+        name: this.state.name,
+        phone: this.state.phone,
+      });
+    } else {
+      this.setState({
+        name: info.name,
+        phone: info.phone,
+      });
+    }
+    this.setState({
+      editing: !this.state.editing,
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+      // name이 target의 value값이 된다.
+    });
   };
 
   render() {
     const { name, phone, id } = this.props.info;
     // 비구조 할당을 통한 props로 데이터를 전달받는다.
+    const { editing } = this.state;
+    // 레퍼런스 끌어오기
+
     const style = {
       border: "1px solid black",
       padding: "8px",
@@ -26,12 +57,38 @@ class PhoneInfo extends Component {
     };
     return (
       <div style={style}>
-        <div>
-          <b>{name}</b>
-        </div>
-        <div>{phone}</div>
+        {editing ? (
+          <Fragment>
+            {/* div 대용으로 쓰는 Fragment */}
+            <div>
+              <input
+                name="name"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+            </div>
+            <div>
+              <input
+                name="phone"
+                onChange={this.handleChange}
+                value={this.state.phone}
+              />
+            </div>
+            {/* inline 대신 block 으로 표현하기 위해 div로 감싸기 */}
+          </Fragment>
+        ) : (
+          <Fragment>
+            <div>
+              <b>{name}</b>
+            </div>
+            <div>{phone}</div>
+          </Fragment>
+        )}
         <button onClick={this.handleRemove}>Delete</button>
-        <button>edit</button>
+        <button onClick={this.handleToggleEdit}>
+          {editing ? "update" : "edit"}
+          {/* 삼항 연산자를 사용해서 표현 */}
+        </button>
       </div>
     );
   }
