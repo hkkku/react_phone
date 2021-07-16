3c7b171810be2d6182a8b5941a1f3bfab7f5a246
hkkku <hhk7045@gmail.com>
2021-07-16 17:47:14 +0900

    07/16 실습 강의 마무리

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 // 부모 컴포넌트에게 데이터를 전달할 자식 컴포넌트
 class PhoneForm extends Component {
+  // input = null;
+  input = React.createRef();
+  // 필수적인 설정 내용x
   state = {
     name: "",
     phone: "",
@@ -8,39 +11,45 @@ class PhoneForm extends Component {
 
   handleChange = (e) => {
     this.setState({
-      [e.target.name] : e.target.value,
+      [e.target.name]: e.target.value,
       // input의 value를 name 값에 setState로 설정
     });
-  }
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.onCreate(this.state);
     this.setState({
       // 값 초기화
-      name : '',
-      phone: '',
-    })
-  }
+      name: "",
+      phone: "",
+    });
+    // this.input.focus();
+    this.input.current.focus();
+  };
   // 이벤트 객체 조정
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <input
-            name = "name"
+            name="name"
             placeholder="name"
             onChange={this.handleChange}
             value={this.state.name}
+            ref={this.input}
+            // ref={(ref) => (this.input = ref)}
+            // DOM에 직접 접근할 수 있게 해주는 함수
           />
           {/* onChange => input에서 값을 변경할 때마다 name 값이 바뀐다. */}
-          <input 
-            name="phone" 
-            placeholder="phone number" 
+          <input
+            name="phone"
+            placeholder="phone number"
             onChange={this.handleChange}
-            value={this.state.phone} />
+            value={this.state.phone}
+          />
           {/* input이 여러 개 생길 때의 state 관리법 */}
-          <button type ="submit">submit</button>
+          <button type="submit">submit</button>
           {/* <div>
             {this.state.name}
             {this.state.phone}
